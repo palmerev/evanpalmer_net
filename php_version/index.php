341688e106c238d93adbba92d9efb7ef705e1c15
Evan Palmer <evjpalmer@gmail.com>
2015-10-07 20:25:54 -0700

    convert HTML templates to twig templates

--- php_version/index.php
+++ php_version/index.php
@@ -24,11 +24,11 @@ $view->parserExtensions = array(
 );
 
 $app->get('/', function() use($app){
-  $app->render('index.html');
+  $app->render('index.twig');
 });
 
 $app->get('/contact', function() use($app){
-  $app->render('contact.html');
+  $app->render('contact.twig');
 });
 
 $app->run();
